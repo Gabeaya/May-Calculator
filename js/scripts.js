435d70af39c4619cd562e951911112a4cd27252d
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-08 19:24:22 -0700

    Add a function that converts celsius to fahrenheit and comment out the number 2 variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+function convert(number1) {
+  return (number1 * 1.8) + 32
+}
 function subtract(number1,number2) {
   return number1 - number2;
 }
@@ -11,7 +14,7 @@ function add(number1, number2) {
   return number1 + number2;
   }
   
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result= add(number1, number2);
-  alert(result);
+  const number1 = parseInt(prompt("Enter a degree in celsius"));
+  //const number2 = parseInt(prompt("Enter another number:"));
+  const result= convert(number1);
+  alert(result + " degrees fahrenheit");
