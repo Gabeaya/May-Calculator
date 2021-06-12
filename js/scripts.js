e159cbe55a14941cae66e6ba106af11139f02941
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-11 22:37:32 -0700

    Add the val method to collect user input.

--- js/scripts.js
+++ js/scripts.js
@@ -14,16 +14,11 @@ function add(number1, number2) {
   return number1 + number2;
   }
   
-  const number1 = parseInt(prompt("Enter a number"));
-  const number2 = parseInt(prompt("Enter another number:"));
+//User Interface
+
+$(document).ready(function() {
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
   const result= add(number1, number2);
   alert(result);
-
-  //User Interface
-
-  $(document).ready(function() {
-    const number1 = parseInt(prompt("Enter a number"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  const result= add(number1, number2);
-  alert(result);
-  })
+});
