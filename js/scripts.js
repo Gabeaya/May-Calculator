784be52c37b87a77eb72ced5bccb791a81fdeaba
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-11 22:42:40 -0700

    create a method that will wait to add forms until user enter them.

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,11 @@ function add(number1, number2) {
 //User Interface
 
 $(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
   const number1 = parseInt($("#add1").val());
   const number2 = parseInt($("#add2").val());
   const result= add(number1, number2);
   alert(result);
+  });
 });
